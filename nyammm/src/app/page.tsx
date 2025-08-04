6731ab357f4dfbc65ad9b6ba6c9fcf7203abe0cd
Farkas Bence <farkas.bence27@gmail.com>
2025-08-04 18:48:30 +0200

    init next js with shadcn

---
+++ nyammm/src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+  return <Button onClick={() => setCount(count+1)}>{count}</Button>;
+}
